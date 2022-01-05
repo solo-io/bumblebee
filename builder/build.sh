d84527398cb6c0b20c4ef5541e521c8c19ecbe70
Lawrence Gadban <lawrence.gadban@solo.io>
2022-01-05 23:51:29 +0000

    strip ebpf elf file during build

--- builder/build.sh
+++ builder/build.sh
@@ -3,3 +3,5 @@ set -eu
 
 clang-13 -g -O2 -target bpf -D__TARGET_ARCH_x86 -Wall -c $1 -o $2
 
+# strip debug sections (see: https://github.com/libbpf/libbpf-bootstrap/blob/94000ca67c5e7be4741c09c435c9ae1777822378/examples/c/Makefile#L65)
+llvm-strip-13 -g $2
